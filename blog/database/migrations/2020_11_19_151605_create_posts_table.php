7a0fcae247ff4fcdfb55bfff3941ee99ac44bb7d
Dimitri M <ibmiti@gmail.com>
2020-11-19 10:30:39 -0500

    updating table with 2 new columns : time stamps

---
+++ blog/database/migrations/2020_11_19_151605_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+
+    // the name of this migration is a bit off, due to the table being previously created and this file will simply update the table
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->timestamps();
+            $table->timestamp('published_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
