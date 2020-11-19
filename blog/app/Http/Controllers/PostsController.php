f7cf4c17f92997bfabf91b441ae2867b9355a6b7
Dimitri M <ibmiti@gmail.com>
2020-11-19 10:12:34 -0500

    further refactor

--- blog/app/Http/Controllers/PostsController.php
+++ blog/app/Http/Controllers/PostsController.php
@@ -12,11 +12,10 @@ class PostsController extends Controller
 {
     public function show($slug)
     {
-        $post = Post::where('slug', $slug)->firstOrFail();
     
         return view('post', 
         [
-            'post' => $post
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]
     );
 
