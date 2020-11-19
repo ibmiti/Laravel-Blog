1446c6d386edcaad9652811270aa60431742b731
Dimitri M <ibmiti@gmail.com>
2020-11-19 08:13:04 -0500

    can now pull from database

--- blog/app/Http/Controllers/PostsController.php
+++ blog/app/Http/Controllers/PostsController.php
@@ -9,22 +9,12 @@ class PostsController extends Controller
 {
     public function show($slug)
     {
-    //     $posts = [ '0' => 'yes i am first',
-    //                 '1' => 'why yes i am second'
-    // ];
     
         $post = \DB::table('posts')->where('slug', $slug)->first();
-
-        dd($post);
-
-        if (! array_key_exists($post, $posts)){
-            // I could return an image here instead of an 404 page
-            abort(404, 'Sorry that post was not found.');
-        }
     
         return view('post', 
         [
-            'post' => $posts[$post]
+            'post' => $post
         ]
     );
 
--- blog/resources/views/post.blade.php
+++ blog/resources/views/post.blade.php
@@ -1,3 +1,3 @@
 <h1>Blog</h1>
 
-<p>{{ $post ?? '' }}</p>
+<p>{{ $post->body }}</p>
