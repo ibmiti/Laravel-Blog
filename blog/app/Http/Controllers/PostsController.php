c503b440df1a6588f251e4c5b9560d6a8ae44f87
Dimitri M <ibmiti@gmail.com>
2020-11-19 10:08:25 -0500

    send

--- blog/app/Http/Controllers/PostsController.php
+++ blog/app/Http/Controllers/PostsController.php
@@ -13,11 +13,7 @@ class PostsController extends Controller
     public function show($slug)
     {
         $post = Post::where('slug', $slug)->firstOrFail();
-        
-        // if (! $post) {
-        //     abort(404);
-        // }
-
+    
         return view('post', 
         [
             'post' => $post
