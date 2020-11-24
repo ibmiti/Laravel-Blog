cac0424d0662272b027956d86e7029a8fb13f063
Dimitri M <ibmiti@gmail.com>
2020-11-24 07:30:40 -0500

    send

--- blog/resources/views/components/navbar2.blade.php
+++ blog/resources/views/components/navbar2.blade.php
@@ -1,27 +1,73 @@
 <div class="container container-fluid">
-  <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">BabyBassinet</a>
-    <ul class="navbar-nav ml-auto"> 
-<?php     
-    if (!Request::is('/'))
-    {
-        echo '<li class="nav-item"><a class="nav-link" href="/">Home</a></li>';
-    }
-    if (!Request::is('about'))
-      {
-        echo '<li class="nav-item"> <a class="nav-link" href="/about">About</a></li>'; 
-      }    
-    if (!Request::is('posts'))
-    {
-        echo '<li class="nav-item">  <a class="nav-link" href="/posts">Blog</a></li>';
-    }
-    if (!Request::is('contact'))
-    {
-        echo '<li class="nav-item"> <a class="nav-link" href="/contact">Contact</a></li>';
-    }
-?>
+<!-- find new color -->
+  <!-- <nav class="navbar sticky-top navbar-expand-lg justify-content-between navbar-light bg-light" role="navigation">  -->
+  <nav class="navbar sticky-top navbar-expand-lg justify-content-between navbar-light "  style="background-color: #EFFDFA;" role="navigation"> 
+    <a class="navbar-brand" href="#">
+    <!-- <img src="https://www.geeksforgeeks.org/wp-content/uploads/gfg_transparent_white_small.png" 
+                         width="30" height="30"
+                         class="d-inline-block align-top" alt="">    will use own image -->
+    <span style="color:blue;">BABY</span>BASSINET
+    </a>
+    <button class="navbar-toggler bg-light" 
+            type="button" 
+            data-toggle="collapse" 
+            data-target="#navbarNavDropdown01"
+            aria-controls="navbarNavDropdown01"
+            aria-expanded="false" 
+            aria-label="Toggle navigation"
+            style="outline-color:pink"> 
+        <span class="navbar-toggler-icon"></span> 
+    </button> 
+
+    <div class="collapse navbar-collapse m-2 p-4"  id="navbarNavDropdown01">
+    <ul class="navbar-nav text-right mr-4"> 
+        <?php     
+            if (!Request::is('/'))
+            {
+                echo ('<li class="nav-item">
+                        <a class="nav-link" href="/">HOME</a>
+                    </li>');
+            } elseif (Request::is('/'))
+            {
+                   echo ('<li class="nav-item active">
+                        <a class="nav-link" href="/">HOME</a>
+                    </li>');
+            } elseif (!Request::is('about'))
+            {
+                echo ('<li class="nav-item">
+                            <a class="nav-link" href="/about">ABOUT</a>
+                        </li>'); 
+            } elseif (Request::is('about'))
+            {
+                   echo ('<li class="nav-item active">
+                        <a class="nav-link" href="/about">ABOUT</a>
+                    </li>');
+            }   
+
+            if (!Request::is('posts'))
+            {
+                echo ('<li class="nav-item">
+                        <a class="nav-link" href="/posts">BLOG</a>
+                    </li>');
+            } elseif (Request::is('posts'))
+            {
+                echo ('<li class="nav-item">
+                        <a class="nav-link" href="/posts">BLOG</a>
+                    </li>');
+            }
+
+            if (!Request::is('contact'))
+            {
+                echo ('<li class="nav-item">
+                        <a class="nav-link" href="/contact">CONTACT</a>
+                    </li>');
+            } elseif (Request::is('contact'))
+            {
+                echo ('<li class="nav-item active">
+                        <a class="nav-link" href="/contact">CONTACT</a>
+                    </li>');
+            }
+        ?>
    </ul> 
   </nav>                             
 </div>
-
-
--- blog/resources/views/contact.blade.php
+++ blog/resources/views/contact.blade.php
@@ -1,5 +1,5 @@
 @extends ('layout')
 
 @section ('content')
-    <h1>Hello world </h1>
+    <h1>Hello contact </h1>
 @endsection
