c3ce2caa4464e1cfc180e86e3c6d1ebb8522b6b9
Dimitri M <ibmiti@gmail.com>
2020-11-25 05:16:21 -0500

    adding styles

--- blog/resources/views/components/navbar.blade.php
+++ blog/resources/views/components/navbar.blade.php
@@ -1,24 +1,11 @@
-<style>
-    @media screen and (max-width: 767px){
- 
-        #logo {
-            width : 300px;
-            height :250px;
-        }
-	}   
-</style>
 <div class="container container-fluid">
-<!-- find new color -->
-  <!-- <nav class="navbar sticky-top navbar-expand-lg justify-content-between navbar-light bg-light" role="navigation">  -->
   <nav class="navbar sticky-top navbar-expand-lg justify-content-between navbar-light"  style="background-color: #EFFDFA;" role="navigation"> 
+  
     <a id="logo" class="navbar-brand" href="/">
-
-    <img  id="logo" src="images/logo_babybassinet.png" class="d-inline-block align-to"/> 
-                         
+        <img src="images/bbLogo.png" class="d-inline-block align-to" width="120" height="100" style="padding-top:15px"/>        
+        BABYBASSINET          
     </a>
-
-
-    <button class="navbar-toggler bg-light" 
+    <button id="hamburger" class="navbar-toggler bg-light" 
             type="button" 
             data-toggle="collapse" 
             data-target="#navbarNavDropdown01"
@@ -26,7 +13,7 @@
             aria-expanded="false" 
             aria-label="Toggle navigation"
             style="outline-color:pink"> 
-        <span class="navbar-toggler-icon"></span> 
+        <span style="" class="navbar-toggler-icon"></span> 
     </button> 
 
     <div class="collapse navbar-collapse m-2 p-4"  id="navbarNavDropdown01">
@@ -45,8 +32,7 @@
                     echo ('<li class="nav-item active"><a class="nav-link" href="/posts">BLOG</a></li>');
                     echo ('<li class="nav-item"><a class="nav-link" href="/contact">CONTACT</a></li>');
                     break;
-                case ($uri == '/about'):
-                    
+                case ($uri == '/about'):    
                     echo ('<li class="nav-item"><a class="nav-link" href="/">HOME</a></li>');
                     echo ('<li class="nav-item active"><a class="nav-link" href="/about">ABOUT</a></li>');
                     echo ('<li class="nav-item"><a class="nav-link" href="/posts">BLOG</a></li>');
