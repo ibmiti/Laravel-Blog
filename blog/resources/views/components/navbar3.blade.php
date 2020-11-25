c8fdbbf03b6ab658e0f368b4dca362cc4a88dee3
Dimitri M <ibmiti@gmail.com>
2020-11-25 13:05:08 -0500

    send

--- blog/resources/views/components/navbar3.blade.php
+++ blog/resources/views/components/navbar3.blade.php
@@ -1,48 +1,20 @@
-<style>
-    .navbar.navbar-1 .navbar-toggler-icon {
-background-image: url('https://mdbootstrap.com/img/svg/hamburger6.svg?color=000');
-}
-.navbar.navbar-2 .navbar-toggler-icon {
-background-image: url('https://mdbootstrap.com/img/svg/hamburger2.svg?color=fff');
-}
-.navbar.navbar-3 .navbar-toggler-icon {
-background-image: url('https://mdbootstrap.com/img/svg/hamburger1.svg?color=6a1b9a');
-}
-.navbar.navbar-4 .navbar-toggler-icon {
-background-image: url('https://mdbootstrap.com/img/svg/hamburger7.svg?color=BFE100');
-}
-.navbar.navbar-5 .navbar-toggler-icon {
-background-image: url('https://mdbootstrap.com/img/svg/hamburger5.svg?color=f3e5f5');
-}
-.navbar.navbar-6 .navbar-toggler-icon {
-background-image: url('https://mdbootstrap.com/img/svg/hamburger8.svg?color=E3005C');
-}
-.navbar.navbar-7 .navbar-toggler-icon {
-background-image: url('https://mdbootstrap.com/img/svg/hamburger9.svg?color=FF2C00');
-}
-.navbar.navbar-8 .navbar-toggler-icon {
-background-image: url('https://mdbootstrap.com/img/svg/hamburger4.svg?color=1729B0');
-}
-.navbar.navbar-9 .navbar-toggler-icon {
-background-image: url('https://mdbootstrap.com/img/svg/hamburger3.svg?color=00FBD8');
-}
-
-</style>
-
-
-
-
-<!--Navbar-->
-
-<nav class="navbar navbar-light navbar-4 white">
+<nav class="navbar navbar-light navbar white">
 <a id="logo" href="/">
-        <img src="images/bbLogo.png" class="navbar-brand d-inline-block align-to waves-effect" width="60" height="60" style="padding-top:15px"/>   
+        <img src="images/bbLogo.png" class="navbar-brand d-inline-block align-to waves-effect" width="60" height="80" style="padding-top:15px"/>   
         <strong class="navbar-brand" href="#">BABY BASSINET</strong>   
 </a>
 
   <!-- Collapse button -->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent15"
-    aria-controls="navbarSupportedContent15" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+  <button class="navbar-toggler" 
+          type="button" 
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent15"
+          aria-controls="navbarSupportedContent15"
+          aria-expanded="false" 
+          aria-label="Toggle navigation"
+          style="border: none;outline: none;"
+          >
+    <span class="navbar-toggler-icon"></span></button>
 
   <!-- Collapsible content -->
   <div class="collapse navbar-collapse" id="navbarSupportedContent15">
@@ -78,12 +50,5 @@ background-image: url('https://mdbootstrap.com/img/svg/hamburger3.svg?color=00FB
                 }
         ?>
    </ul> 
-    <!-- Links -->
-
   </div>
-  <!-- Collapsible content -->
-
 </nav>
-<!--/.Navbar-->
-
-<!--Navbar-->
