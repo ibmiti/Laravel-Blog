1ef99dc4cb4e0082e8b083ec6796007b51c432d6
Dimitri M <ibmiti@gmail.com>
2020-11-25 06:00:23 -0500

    footer creation

--- blog/resources/views/components/footer.blade.php
+++ blog/resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <style>
 
-/* footer a {
+footer a {
     text-decoration: none;
     color: white;
 
@@ -10,24 +10,64 @@ footer {
   background: #333;
   color: white;
   padding: 3em;
-
+  width: 100%;
   margin-top: auto;
 
-} */
+}
 
 </style>
 
-<div class="container">
-<footer>
-  <div class="footer__inner">
-    <div class="logo">BabiBassinet</div>
-      <a href="">© Babi Bassinet</a>
-      <div class="social">
-        <a href="">Facebook</a>
-        <a href="">Twitter</a>
-        <a href="">LinkedIn</a>
-        <a href="">Instagram</a>
+<footer class="page-footer font-small cyan darken-3">
+
+  <!-- Footer Elements -->
+  <div class="container">
+
+    <!-- Grid row-->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-12 py-5">
+        <div class="mb-5 flex-center">
+
+          <!-- Facebook -->
+          <a class="fb-ic">
+            <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <!-- Twitter -->
+          <a class="tw-ic">
+            <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <!-- Google +-->
+          <a class="gplus-ic">
+            <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <!--Linkedin -->
+          <a class="li-ic">
+            <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <!--Instagram-->
+          <a class="ins-ic">
+            <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <!--Pinterest-->
+          <a class="pin-ic">
+            <i class="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+          </a>
+        </div>
       </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row-->
+
   </div>
+  <!-- Footer Elements -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+
 </footer>
-</div>
+<!-- Footer -->
